23cb8e9db7c6d9abfb784716b13c8870ffa83fcb
Sumire Doi <m245461@hiroshima-u.ac.jp>
2024-10-27 11:20:22 +0900

    トップ画像の修正

--- front/coffee/lib/screens/slider_screen.dart
+++ front/coffee/lib/screens/slider_screen.dart
@@ -76,198 +76,209 @@ class SliderScreenState extends State<SliderScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('コーヒー診断')),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(height: 20),
-
-            // ICE/HOTボタン
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton.icon(
-                  icon: const Icon(Icons.ac_unit, color: Colors.white),
-                  label: const Text('ICE'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: isIce ? Colors.blue[900] : Colors.blue,
-                    foregroundColor: Colors.white,
-                    side: isIce
-                        ? const BorderSide(color: Colors.black, width: 3)
-                        : null,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      isIce = true;
-                    });
-                  },
-                ),
-                ElevatedButton.icon(
-                  icon: const Icon(Icons.local_fire_department,
-                      color: Colors.white),
-                  label: const Text('HOT'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: isIce ? Colors.red : Colors.red[900],
-                    foregroundColor: Colors.white,
-                    side: isIce
-                        ? null
-                        : const BorderSide(color: Colors.black, width: 3),
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      isIce = false;
-                    });
-                  },
-                ),
-              ],
+      body: Stack(
+        children: [
+          // 背景画像
+          SizedBox.expand(
+            child: Image.asset(
+              'images/background.jpg',
+              fit: BoxFit.cover,
             ),
-            const SizedBox(height: 20),
-
-            // テイストバランススライダー
-            const Text('テイストバランス'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          ),
+          // 背景の上に表示される内容
+          SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                const Text('酸味'),
-                Expanded(
-                  child: Slider(
-                    value: sliderValue1.toDouble(),
-                    min: 0,
-                    max: 100,
-                    divisions: 100,
-                    onChanged: (value) {
-                      setState(() {
-                        sliderValue1 = value.toInt();
-                      });
-                    },
-                  ),
-                ),
-                const Text('苦味'),
-              ],
-            ),
-            const SizedBox(height: 20),
+                const SizedBox(height: 20),
 
-            // ボディスライダー
-            const Text('ボディ'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('ライト'),
-                Expanded(
-                  child: Slider(
-                    value: sliderValue2.toDouble(),
-                    min: 0,
-                    max: 100,
-                    divisions: 100,
-                    onChanged: (value) {
-                      setState(() {
-                        sliderValue2 = value.toInt();
-                      });
-                    },
-                  ),
-                ),
-                const Text('フル'),
-              ],
-            ),
-            const SizedBox(height: 20),
-
-            // ローストスライダー
-            const Text('ロースト'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('浅煎り'),
-                Expanded(
-                  child: Slider(
-                    value: sliderValue3.toDouble(),
-                    min: 0,
-                    max: 100,
-                    divisions: 100,
-                    onChanged: (value) {
-                      setState(() {
-                        sliderValue3 = value.toInt();
-                      });
-                    },
-                  ),
-                ),
-                const Text('深煎り'),
-              ],
-            ),
-            const SizedBox(height: 20),
-
-            // こだわりチェックボックス
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('こだわりますか'),
-                Checkbox(
-                  value: isPro,
-                  onChanged: (value) {
-                    setState(() {
-                      isPro = value!;
-                    });
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-
-            // カテゴリー選択（こだわる場合のみ表示）
-            Visibility(
-              visible: isPro,
-              child: Wrap(
-                runSpacing: 16,
-                spacing: 16,
-                children: category.map((tag) {
-                  final isSelected = selectedCategory.contains(tag);
-                  return InkWell(
-                    borderRadius: const BorderRadius.all(Radius.circular(32)),
-                    onTap: () {
-                      setState(() {
-                        if (isSelected) {
-                          selectedCategory.remove(tag); // 選択されている場合は削除
-                        } else {
-                          selectedCategory.add(tag); // 選択されていない場合は追加
-                        }
-                      });
-                    },
-                    child: AnimatedContainer(
-                      duration: const Duration(milliseconds: 200),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 8),
-                      decoration: BoxDecoration(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(32)),
-                        border: Border.all(
-                          width: 2,
-                          color: Colors.pink,
-                        ),
-                        color: isSelected ? Colors.pink : null,
+                // ICE/HOTボタン
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton.icon(
+                      icon: const Icon(Icons.ac_unit, color: Colors.white),
+                      label: const Text('ICE'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: isIce ? Colors.blue[900] : Colors.blue,
+                        foregroundColor: Colors.white,
+                        side: isIce
+                            ? const BorderSide(color: Colors.black, width: 3)
+                            : null,
                       ),
-                      child: Text(
-                        tag,
-                        style: TextStyle(
-                          color: isSelected ? Colors.white : Colors.pink,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      onPressed: () {
+                        setState(() {
+                          isIce = true;
+                        });
+                      },
+                    ),
+                    ElevatedButton.icon(
+                      icon: const Icon(Icons.local_fire_department,
+                          color: Colors.white),
+                      label: const Text('HOT'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: isIce ? Colors.red : Colors.red[900],
+                        foregroundColor: Colors.white,
+                        side: isIce
+                            ? null
+                            : const BorderSide(color: Colors.black, width: 3),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          isIce = false;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                // テイストバランススライダー
+                const Text('テイストバランス'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('酸味'),
+                    Expanded(
+                      child: Slider(
+                        value: sliderValue1.toDouble(),
+                        min: 0,
+                        max: 100,
+                        divisions: 100,
+                        onChanged: (value) {
+                          setState(() {
+                            sliderValue1 = value.toInt();
+                          });
+                        },
                       ),
                     ),
-                  );
-                }).toList(),
-              ),
-            ),
-            const SizedBox(height: 20),
+                    const Text('苦味'),
+                  ],
+                ),
+                const SizedBox(height: 20),
 
-            // 診断開始ボタン
-            ElevatedButton(
-              onPressed: () async {
-                await sendValues(context);
-              },
-              child: const Text('診断開始'),
+                // ボディスライダー
+                const Text('ボディ'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('ライト'),
+                    Expanded(
+                      child: Slider(
+                        value: sliderValue2.toDouble(),
+                        min: 0,
+                        max: 100,
+                        divisions: 100,
+                        onChanged: (value) {
+                          setState(() {
+                            sliderValue2 = value.toInt();
+                          });
+                        },
+                      ),
+                    ),
+                    const Text('フル'),
+                  ],
+                ),
+                const SizedBox(height: 20),
+
+                // ローストスライダー
+                const Text('ロースト'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('浅煎り'),
+                    Expanded(
+                      child: Slider(
+                        value: sliderValue3.toDouble(),
+                        min: 0,
+                        max: 100,
+                        divisions: 100,
+                        onChanged: (value) {
+                          setState(() {
+                            sliderValue3 = value.toInt();
+                          });
+                        },
+                      ),
+                    ),
+                    const Text('深煎り'),
+                  ],
+                ),
+                const SizedBox(height: 20),
+
+                // こだわりチェックボックス
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('こだわりますか'),
+                    Checkbox(
+                      value: isPro,
+                      onChanged: (value) {
+                        setState(() {
+                          isPro = value!;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+
+                // カテゴリー選択（こだわる場合のみ表示）
+                Visibility(
+                  visible: isPro,
+                  child: Wrap(
+                    runSpacing: 16,
+                    spacing: 16,
+                    children: category.map((tag) {
+                      final isSelected = selectedCategory.contains(tag);
+                      return InkWell(
+                        borderRadius: const BorderRadius.all(Radius.circular(32)),
+                        onTap: () {
+                          setState(() {
+                            if (isSelected) {
+                              selectedCategory.remove(tag); // 選択されている場合は削除
+                            } else {
+                              selectedCategory.add(tag); // 選択されていない場合は追加
+                            }
+                          });
+                        },
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 200),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 8),
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(32)),
+                            border: Border.all(
+                              width: 2,
+                              color: Colors.pink,
+                            ),
+                            color: isSelected ? Colors.pink : null,
+                          ),
+                          child: Text(
+                            tag,
+                            style: TextStyle(
+                              color: isSelected ? Colors.white : Colors.pink,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                const SizedBox(height: 20),
+
+                // 診断開始ボタン
+                ElevatedButton(
+                  onPressed: () async {
+                    await sendValues(context);
+                  },
+                  child: const Text('診断開始'),
+                ),
+                const SizedBox(height: 20),
+              ],
             ),
-            const SizedBox(height: 20),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
